f46c88e6c2d877aeb0f354b674a4e27dc9f7bc08
Your Name <you@example.com>
2018-10-17 19:45:31 +0800

    Site updated: 2018-10-17 19:45:31

--- ros-docker/rviz.sh
+++ ros-docker/rviz.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# xhost +
-if [ "$1" == "rviz" ];then
+xhost +
+# if [ "$1" == "rviz" ];then
   echo "rviz"
   docker run -it \
     --add-host=ilife:192.168.0.1 \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v ~/github/hexo/source/_posts/ros-docker/default.rviz:/root/.rviz/default.rviz \
-  ilife:ros \
-  bash -c "rosrun rviz rviz /root/.rviz/default.rviz"
-fi 
+  carto:laster \
+bash -c "rosrun rviz rviz /root/.rviz/default.rviz"
+#  bash -c "roslaunch /opt/cartographer_ros/share/cartographer_ros/launch/demo_revo_lds.launch"
+
+  
+# fi 
 #http://wiki.ros.org/docker/Tutorials/GUI 
 #http://wiki.ros.org/docker/Tutorials/Hardware%20Acceleration#Nvidia 
 #https://github.com/yeasy/docker_practice/blob/master/image/dockerfile/copy.md
